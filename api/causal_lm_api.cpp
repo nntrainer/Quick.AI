EunjuYang/nntrainer

[CausalLM] reset g_chat_template when tokenizer_config.json is absent
Reset the static g_chat_template to a default (unavailable) instance
when the new model directory does not contain tokenizer_config.json.
Previously, a template from a prior loadModel() call would leak into
subsequent model loads, causing incorrect prompt formatting.

Signed-off-by: Eunju Yang <ej.yang@samsung.com>

Applications/CausalLM/api/causal_lm_api.cpp
@@ -488,6 +488,7 @@ ErrorCode loadModel(BackendType compute, ModelType modeltype,
           << std::endl;
       }
     } else {
+      g_chat_template = causallm::ChatTemplate();
       std::cerr << "[Warning] tokenizer_config.json not found in "
                 << model_dir_path << ". Using hardcoded chat templates."
                 << std::endl;

nntrainer/Quick.AI

api/causal_lm_api.cpp
@@ -488,6 +488,7 @@ ErrorCode loadModel(BackendType compute, ModelType modeltype,
           << std::endl;
       }
     } else {
+      g_chat_template = quick_dot_ai::ChatTemplate();
       std::cerr << "[Warning] tokenizer_config.json not found in "
                 << model_dir_path << ". Using hardcoded chat templates."
                 << std::endl;